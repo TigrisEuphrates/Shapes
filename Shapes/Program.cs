012e1b0ed352ecfe061b7e90da3c1676a71b2830
Tigris <tabula.tenebris@gmail.com>
2021-09-24 15:05:57 +0600

    Add project files.

---
+++ Shapes/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hexagon hex = new Hexagon("Beth");
+            hex.Draw();
+
+            Circle cir = new Circle("Cindy");
+            cir.Draw();
+            Console.WriteLine();
+
+            Shape[] myShapes = new Shape[] {new Hexagon(),new Circle(), new Hexagon("Mick"),
+                                            new Circle("Beth"), new Hexagon("Linda") };
+            foreach (var s in myShapes)
+            {
+                s.Draw();
+            }
+            Console.WriteLine();
+
+            ThreeDCircle o = new ThreeDCircle();
+            o.Draw();
+            ((Circle)o).Draw();
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Shapes/Shape.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes
+{
+    abstract class Shape
+    {
+        public string PetName { get; set; }
+        public Shape(string name="NoName")
+        {
+            PetName = name;
+        }
+        abstract public void Draw();
+        //virtual public void Draw()
+        //{
+        //    Console.WriteLine("Inside Shape.Draw()");
+        //}
+    }
+}
